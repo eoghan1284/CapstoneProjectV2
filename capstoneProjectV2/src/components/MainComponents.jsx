b96f9e28de463f8dd59b7919ae2358eecbec667d
Eoghan Clancy <eoghan1284@gmail.com>
2024-01-31 15:13:00 +0000

    testDiv working

---
+++ capstoneProjectV2/src/components/MainComponents.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+//import WelcomePage from './components/WelcomePage';
+//import Survey1 from './components/Survey1';
+//import Survey2 from './components/Survey2';
+//import Survey3 from './components/Survey3';
+//import Game from './components/Game';
+import '../styles/App.css';
+
+const MainComponents = () => {
+    console.log("maincomponents");
+    return <div id='testDiv'>
+                <p>testDiv</p>
+            </div>
+  //const [currentStep, setCurrentStep] = useState('welcome');
+  // ... rest of your state and handlers
+
+  // ... rest of your component with conditional rendering
+};
+
+export default MainComponents;

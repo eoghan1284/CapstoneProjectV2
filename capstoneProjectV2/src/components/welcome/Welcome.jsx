7069eb806f334878a645c0ddc5289370a8224864
Eoghan Clancy <eoghan1284@gmail.com>
2024-02-01 10:56:55 +0000

    Completed first page visuals (Welcome page)

--- capstoneProjectV2/src/components/welcome/Welcome.jsx
+++ capstoneProjectV2/src/components/welcome/Welcome.jsx
@@ -1,9 +1,22 @@
 import './Welcome.css'
 
 const Welcome = ({ onStart }) => (
-    <div id="Welcome_testDiv">
-      <h1>Welcome</h1>
-      <button onClick={onStart}>Start</button>
+    <div id="page">
+      <div id='container'>
+        <div id='titleDiv'>
+          <h2>Signup</h2>
+        </div>
+        <div id ='inputDiv'>
+          <input id='usernameInput' type="text" placeholder='Username'/>
+          <select id='genderInput' name='Gender'>
+            <option value="" disabled selected>Select your gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+
+          <button onClick={onStart}>Start</button>
+        </div>
+      </div>
     </div>
   );
   

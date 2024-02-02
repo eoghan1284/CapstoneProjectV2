4a7606a0c16c79161b4c84fce064e9cf9d226698
Eoghan Clancy <eoghan1284@gmail.com>
2024-02-02 08:24:33 +0000

    Created Cave jsx/css but not implemented

---
+++ capstoneProjectV2/src/components/gameComponents/Cave.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles/Cave.css'; 
+
+/*const handleCaveClick = () => {
+    console.log("cave clicked");
+  };
+*/
+
+const Cave = () => {
+    return (
+        <div id='container'/* onClick={() => handleCaveClick()}*/>
+            <div>
+                CAVE
+            </div>
+        </div>
+    );
+};
+
+export default Cave;
+

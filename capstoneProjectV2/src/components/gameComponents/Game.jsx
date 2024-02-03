d09e64a233a8548e9f185e706a72f5a5dbfe4d3a
Eoghan Clancy <eoghan1284@gmail.com>
2024-02-03 10:55:06 +0000

    Created seperate cave components in game.jsx with inline styles and replaced child divs with them

--- capstoneProjectV2/src/components/gameComponents/Game.jsx
+++ capstoneProjectV2/src/components/gameComponents/Game.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './styles/Game.css'; 
-import backgroundImage from './gameAssets/BackgroundSave1_2.png';
-import Cave from './Cave.jsx'; //this import breaks welcome page????
 
 function Game( {onGameEnd} ) {
 
@@ -11,13 +9,35 @@ function Game( {onGameEnd} ) {
 
   const handlePseudoCaveClick = () => {
     console.log("pseudo cave clicked");
-};
+  };
+
+  const handleCaveClick = () => {
+    console.log("cave clicked");
+  };
+
+  const Cave1 = () => {
+    return (
+      <div onClick={handleCaveClick} style={{ position: 'absolute', top: '70%', left: '2%', height: '10vh', width: '8vh', opacity: '0.5', backgroundColor: 'purple' }}></div>
+    );
+  }
+
+  const Cave2 = () => {
+    return (
+      <div onClick={handleCaveClick} style={{ position: 'absolute', top: '55%', left: '12%', height: '9vh', width: '8vh', opacity: '0.5', backgroundColor: 'yellow' }}></div>
+    );
+  }
+
+  const Cave3 = () => {
+    return (
+      <div onClick={handleCaveClick} style={{ position: 'absolute', top: '57%', left: '83%', height: '14vh', width: '9vh', opacity: '0.5', backgroundColor: 'red' }}></div>
+    );
+  }
 
   return (
       <div className="parent">
-        <div className="child" id="child1" onClick={handlePseudoCaveClick}></div>
-        <div className="child" id="child2" onClick={handlePseudoCaveClick}></div>
-        <div className="child" id="child3" onClick={handlePseudoCaveClick}></div>
+        <Cave1></Cave1>
+        <Cave2></Cave2>
+        <Cave3></Cave3>
       </div>
   );
 }

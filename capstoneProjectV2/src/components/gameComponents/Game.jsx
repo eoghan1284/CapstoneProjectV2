1390319f03d7c8d469ce7cc5b0ee17fd743de086
Eoghan Clancy <eoghan1284@gmail.com>
2024-02-04 09:41:13 +0000

    Make cave divs update gold counter

--- capstoneProjectV2/src/components/gameComponents/Game.jsx
+++ capstoneProjectV2/src/components/gameComponents/Game.jsx
@@ -18,26 +18,31 @@ function Game( {onGameEnd} ) {
 
   const probs = [0,1,0.5]
 
-  //to do: make a counter for gold using useState and make it update based on results of winLoss (cave click)
+  
+  const [playerGold, setPlayerGold] = useState(100);
+
+  const updatePlayerGold = (res) => {
+    setPlayerGold(prevCount => prevCount + res);
+  };
 
   const Cave1 = () => {
     
     return (
-      <div onClick={() => handleCaveClick(1, winLoss(probs[0]))} style={{ position: 'absolute', top: '70%', left: '2%', height: '10vh', width: '8vh', opacity: '0.5', backgroundColor: 'purple' }}></div>
+      <div onClick={() => handleCaveClick(1, updatePlayerGold(winLoss(probs[0])))} style={{ position: 'absolute', top: '70%', left: '2%', height: '10vh', width: '8vh', opacity: '0.5', backgroundColor: 'purple' }}></div>
     );
   }
 
   const Cave2 = () => {
 
     return (
-      <div onClick={() => handleCaveClick(2, winLoss(probs[1]))} style={{ position: 'absolute', top: '55%', left: '12%', height: '9vh', width: '8vh', opacity: '0.5', backgroundColor: 'yellow' }}></div>
+      <div onClick={() => handleCaveClick(2, updatePlayerGold(winLoss(probs[1])))} style={{ position: 'absolute', top: '55%', left: '12%', height: '9vh', width: '8vh', opacity: '0.5', backgroundColor: 'yellow' }}></div>
     );
   }
 
   const Cave3 = () => {
 
     return (
-      <div onClick={() => handleCaveClick(3, winLoss(probs[2]))} style={{ position: 'absolute', top: '56%', left: '85%', height: '15vh', width: '8vh', opacity: '0.5', backgroundColor: 'red' }}></div>
+      <div onClick={() => handleCaveClick(3, updatePlayerGold(winLoss(probs[2])))} style={{ position: 'absolute', top: '56%', left: '85%', height: '15vh', width: '8vh', opacity: '0.5', backgroundColor: 'red' }}></div>
     );
   }
 
@@ -51,7 +56,7 @@ function Game( {onGameEnd} ) {
         </div>
         <div id='hudDiv'>
           <p>Gold</p>
-          <p id='scoreDisplay'>100</p>
+          <p id='scoreDisplay'>{playerGold}</p>
         </div>
       </div>
   );

dabb9d26331a42e89400f370a3b7bd5f2fe99124
Eoghan Clancy <eoghan1284@gmail.com>
2024-02-04 09:57:41 +0000

    ...

--- capstoneProjectV2/src/components/gameComponents/Game.jsx
+++ capstoneProjectV2/src/components/gameComponents/Game.jsx
@@ -26,39 +26,36 @@ function Game( {onGameEnd} ) {
   };
 
   const Cave1 = () => {
-    
     return (
       <div onClick={() => handleCaveClick(1, updatePlayerGold(winLoss(probs[0])))} style={{ position: 'absolute', top: '70%', left: '2%', height: '10vh', width: '8vh', opacity: '0.5', backgroundColor: 'purple' }}></div>
     );
   }
 
   const Cave2 = () => {
-
     return (
       <div onClick={() => handleCaveClick(2, updatePlayerGold(winLoss(probs[1])))} style={{ position: 'absolute', top: '55%', left: '12%', height: '9vh', width: '8vh', opacity: '0.5', backgroundColor: 'yellow' }}></div>
     );
   }
 
   const Cave3 = () => {
-
     return (
       <div onClick={() => handleCaveClick(3, updatePlayerGold(winLoss(probs[2])))} style={{ position: 'absolute', top: '56%', left: '85%', height: '15vh', width: '8vh', opacity: '0.5', backgroundColor: 'red' }}></div>
     );
   }
 
-  return (
-      <div id='gamePage'>
-        <div className="parent">
-          <Cave1></Cave1>
-          <Cave2></Cave2>
-          <Cave3></Cave3>
-          <button onClick={handleGameEnd}>NextPage</button>
-        </div>
-        <div id='hudDiv'>
-          <p>Gold</p>
-          <p id='scoreDisplay'>{playerGold}</p>
-        </div>
+return (
+    <div id='gamePage'>
+      <div className="parent">
+        <Cave1></Cave1>
+        <Cave2></Cave2>
+        <Cave3></Cave3>
+        <button onClick={handleGameEnd}>NextPage</button>
       </div>
+      <div id='hudDiv'>
+        <p>Gold</p>
+        <p id='scoreDisplay'>{playerGold}</p>
+      </div>
+    </div>
   );
 }
 

6c652e0dffc22aeb8bea86e41314a9c440b4a4f6
Eoghan Clancy <eoghan1284@gmail.com>
2024-02-09 11:24:14 +0000

    Added gender state

--- capstoneProjectV2/src/components/welcome/Welcome.jsx
+++ capstoneProjectV2/src/components/welcome/Welcome.jsx
@@ -1,24 +1,41 @@
-import './Welcome.css'
+import React, { useState } from 'react';
+import './Welcome.css';
 
-const Welcome = ({ onStart }) => (
+const Welcome = ({ onStart }) => {
+
+  const [gender, setGender] = useState('');
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+  };
+
+  const handleStart = () => {
+    onStart(); //can pass gender here
+  };
+
+  return (
     <div id="page">
       <div id='container'>
         <div id='titleDiv'>
-          <h2>Signup</h2>
+          <h2>Welcome</h2>
         </div>
         <div id='inputDiv'>
-          <input id='usernameInput' type="text" placeholder='Username' />
-          <select id='genderInput' name='Gender' defaultValue="">
+          <div id='expDiv'>
+            <p>Over the course of this short activity, you will fill out 3 short surveys and then play a game. <br />
+              Please enter your gender and press Start to begin.
+            </p>
+          </div>
+          <select id='genderInput' name='Gender' value={gender} onChange={handleGenderChange} defaultValue="">
             <option value="" disabled>Select your gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
           </select>
           <div>
-            <button onClick={onStart}>Start</button>
+            <button onClick={handleStart}>Start</button>
           </div>
         </div>
       </div>
     </div>
   );
-  
+};
+
 export default Welcome;

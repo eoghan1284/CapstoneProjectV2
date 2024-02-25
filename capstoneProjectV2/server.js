67d61b361faa8562f127365a52b13af40f242b15
Eoghan Clancy <eoghan1284@gmail.com>
2024-02-25 14:05:35 +0000

    ...

--- capstoneProjectV2/server.js
+++ capstoneProjectV2/server.js
@@ -30,32 +30,3 @@ app.post('/observation',async(req,res) => {
         res.status(500).json({message: error.message})
     }
 })
-
-/*
-this is from model.js just to look at, should be removed when done
-import mongoose from 'mongoose';
-
-
-const dataSchema = mongoose.Schema({
-    survey1Total: {
-        type: Number, 
-        required: [true, "enter survey 1 total"]
-    },
-    survey2Total: {
-        type: Number, 
-        required: [true, "enter survey 2 total"]
-    },
-    survey3Total: {
-        type: Number, 
-        required: [true, "enter survey 3 total"]
-    },
-    probabilities: {
-        type: [Number], // an array of 3 numbers eg 0.3, 0.5, 0.2
-        required: [true, "enter probabilities"]
-    },
-    finalGold: {
-        type: Number, //playerGold at end
-        required: [true, "enter finalGold"]
-    }
-}
-*/
